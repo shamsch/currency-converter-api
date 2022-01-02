acb19d7b6b87043b2b59f446e56f09fb4eb92490
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-02 15:25:59 +0200

    Boiler plate code ready

---
+++ src/components/Converter.js
@@ -0,0 +1,12 @@
+import DisplayExchangeRate from "./DisplayExchangeRate";
+
+function Converter() {
+  return (
+    <div className="converter">
+      <p>This is the converter!</p>
+      <DisplayExchangeRate/>
+    </div>
+  );
+}
+
+export default Converter;
---
+++ src/components/DisplayExchangeRate.js
@@ -0,0 +1,10 @@
+
+function DisplayExchangeRate() {
+  return (
+    <div className="exchange-rate">
+      <p>Display exchange rate here!</p>
+    </div>
+  );
+}
+
+export default DisplayExchangeRate;
