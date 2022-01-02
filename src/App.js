85af53d397de4bd1f0737ce91396fbf1625be9d4
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-02 17:15:11 +0200

    Loads all currency options from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import DisplayExchangeRate from './components/DisplayExchangeRate';
 import Converter from './components/Converter';
 
 function App() {
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,10 +1,54 @@
+import {useState, useEffect} from "react";
 import DisplayExchangeRate from "./DisplayExchangeRate";
-
+import axios from "axios";
 function Converter() {
+  const [currencies, setCurrencies]=useState({})
+
+  const key = "15d740da79491ba97eb0247056657ffe4c785dbd";
+
+  const listURL =  `https://api.getgeoapi.com/v2/currency/list?api_key=${key}&format=json`
+
+  useEffect(() => {
+    axios.get(`${listURL}/dshhgkhk`).then((response) => {
+      console.log(response.data.currencies);
+      setCurrencies(response.data.currencies)
+    }).catch(error => {
+      console.log("error is "+error);
+    });
+  }, []);
+
   return (
     <div className="converter">
-      <p>This is the converter!</p>
-      <DisplayExchangeRate/>
+      <h1>Convert currency!</h1>
+      <div className="input-area">
+        <table>
+          <tbody>
+            <tr>
+              <td>From currency</td>
+              <td>
+                <input type="number" value={""} name="from-curr-input"></input>
+              </td>
+              <td>
+                <select className="curr-option" value={""} name="from-curr-options">
+                  {Object.values(currencies).sort().map((curr, _index) => <option key={_index}>{curr}</option>)}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>To currency</td>
+              <td>
+                <input type="number" value={""} name="to-curr-input"></input>
+              </td>
+              <td>
+                <select className="curr-option" value={""} name="to-curr-options">
+                  {Object.values(currencies).sort().map((curr, _index) => <option key={_index}>{curr}</option>)}
+                </select>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <DisplayExchangeRate />
     </div>
   );
 }
