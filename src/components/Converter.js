68293d532b2e93c734b55d41d1c2a4b010f8a2d3
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-02 18:14:35 +0200

    convert function partially implemeted

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,21 +1,41 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import DisplayExchangeRate from "./DisplayExchangeRate";
 import axios from "axios";
 function Converter() {
-  const [currencies, setCurrencies]=useState({})
+  const [currencies, setCurrencies] = useState({});
+  const [fromCurr, setFromCurr] = useState("Afghan afghani");
+  const [toCurr, setToCurr] = useState("Afghan afghani");
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
   const key = "15d740da79491ba97eb0247056657ffe4c785dbd";
-
-  const listURL =  `https://api.getgeoapi.com/v2/currency/list?api_key=${key}&format=json`
-
   useEffect(() => {
-    axios.get(`${listURL}/dshhgkhk`).then((response) => {
-      console.log(response.data.currencies);
-      setCurrencies(response.data.currencies)
-    }).catch(error => {
-      console.log("error is "+error);
-    });
+    const listURL = `https://api.getgeoapi.com/v2/currency/list?api_key=${key}&format=json`;
+    axios
+      .get(`${listURL}`)
+      .then((response) => {
+        console.log(response.data.currencies);
+        setCurrencies(response.data.currencies);
+      })
+      .catch((error) => {
+        console.log("error is " + error);
+      });
   }, []);
+
+  const convert = () => {
+    const fromCurrCode= Object.keys(currencies).find(key => currencies[key] === fromCurr)
+    const toCurrCode= Object.keys(currencies).find(key => currencies[key] === toCurr)
+    const conversionURL = `https://api.getgeoapi.com/v2/currency/convert?api_key=${key}&from=${fromCurrCode}&to=${toCurrCode}&amount=${amount}&format=json`;
+    console.log(conversionURL)
+    axios
+      .get(`${conversionURL}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log("error while conversion is " + error);
+      });
+  };
 
   return (
     <div className="converter">
@@ -26,27 +46,58 @@ function Converter() {
             <tr>
               <td>From currency</td>
               <td>
-                <input type="number" value={""} name="from-curr-input"></input>
+                <input
+                  type="number"
+                  value={amount}
+                  name="from-curr-input"
+                  onChange={(e) => setAmount(e.target.value)}
+                ></input>
               </td>
               <td>
-                <select className="curr-option" value={""} name="from-curr-options">
-                  {Object.values(currencies).sort().map((curr, _index) => <option key={_index}>{curr}</option>)}
+                <select
+                  className="curr-option"
+                  value={fromCurr}
+                  name="from-curr-options"
+                  onChange={(e) => setFromCurr(e.target.value)}
+                >
+                  {Object.values(currencies)
+                    .sort()
+                    .map((curr, _index) => (
+                      <option key={_index}>{curr}</option>
+                    ))}
                 </select>
               </td>
             </tr>
             <tr>
               <td>To currency</td>
               <td>
-                <input type="number" value={""} name="to-curr-input"></input>
+                <input
+                  type="number"
+                  value={convertedAmount}
+                  name="to-curr-input"
+                  disabled={true}
+                ></input>
               </td>
               <td>
-                <select className="curr-option" value={""} name="to-curr-options">
-                  {Object.values(currencies).sort().map((curr, _index) => <option key={_index}>{curr}</option>)}
+                <select
+                  className="curr-option"
+                  value={toCurr}
+                  name="to-curr-options"
+                  onChange={(e) => setToCurr(e.target.value)}
+                >
+                  {Object.values(currencies)
+                    .sort()
+                    .map((curr, _index) => (
+                      <option key={_index}>{curr}</option>
+                    ))}
                 </select>
               </td>
             </tr>
           </tbody>
         </table>
+        <button className="convert-btn" onClick={convert}>
+          Convert
+        </button>
       </div>
       <DisplayExchangeRate />
     </div>
