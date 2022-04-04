8b1ad24af371caba5b23e0a66fd457ffa613a3b8
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-04-04 23:37:01 +0300

    refactored

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
-import Converter from './components/Converter';
+import React, { useEffect } from "react";
+import "./App.css";
+import Converter from "./components/Converter";
+import Navbar from "./UI/Navbar";
+import AppGrid from "./UI/AppGrid";
+import DisplayExchangeRate from "./components/DisplayExchangeRate";
+import { useSelector } from "react-redux";
 
 function App() {
+  const {fromCurr,toCurr,conversionFactor} = useSelector((state)=> state.data)
+  console.log(fromCurr,toCurr,conversionFactor)
+
   return (
     <div>
-      <Converter/>
+      <Navbar>Convert currencies!</Navbar>
+      <AppGrid
+        result={
+          <DisplayExchangeRate
+            rate={conversionFactor}
+            fromCurr={fromCurr}
+            toCurr={toCurr}
+          />
+        }
+        converter = {<Converter/>}
+      />
     </div>
   );
 }
